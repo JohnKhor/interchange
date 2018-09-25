7aee56fc11d81a218af853d6239bdfda39409526
John Khor <0jetpistol0@gmail.com>
2018-09-25 22:16:35 +0800

    Add seeding.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Answer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(Answer::class, 10)->create();
     }
 }
