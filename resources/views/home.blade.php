e81baa20094d24b62fe0832bb1934d964c9ab3c8
John Khor <0jetpistol0@gmail.com>
2018-10-18 11:03:24 +0800

    Fix if statement in homepage view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@ Home
     <div class="jumbotron text-center">
         <h1>Welcome to Interchange</h1>
         <p>This is a simple Q&A site built with Laravel and Bootstrap.</p>
-        @if (auth()->check())
+        @if (!auth()->check())
             <a class="btn btn-primary btn-lg" href="{{ route('register') }}">Register</a>
             <a class="btn btn-success btn-lg" href="{{ route('login') }}">Login</a>
         @endif
